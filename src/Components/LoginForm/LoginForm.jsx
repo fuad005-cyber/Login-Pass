c38e8caa88208fa1df3cf39b0a46d0468d8b7408
Fuad <fuadbores@gmail.com>
2024-02-25 13:19:41 +0400

    Upgrade Login form

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 const LoginForm = () => {
+
+    const [action, setAction] = useState("Login")
+
   return (
     <div className='wrapper'>
       <form action="">
         <h1>Az Cake</h1>
         <div className="input-box">
             <input name='login' type="text" placeholder='İstifadəçi email' required maxLength={35} />
-            <FaUser className='icon'/>
+            <MdEmail className='icon'/>
         </div>
+        {action === 'Sign up'?<div><div className="input-box">
+            <input name='password' type="password" placeholder='Ad və Soyadı' required  maxLength={30}/>
+            <FaUser className='icon'/>
+        </div></div>:<div></div>}
         <div className="input-box">
             <input name='password' type="password" placeholder='Şifrə' required  maxLength={30}/>
             <FaLock className='icon'/>
         </div>
+        
         <div className="remember-forgot">
-            <label><input type="checkbox" />Yadda saxla</label>
-            <a href="#">Şifrəni unutmusunuz?</a>
+        {action ==='Sign up'?<div></div>: <label><input type="checkbox" />Yadda saxla</label>}
+        {action ==='Sign up'?<div></div>: <a href="#">Şifrəni unutmusunuz?</a>}
+            
         </div>
-        <button type='submit'>Daxil ol</button>
+       
+        {action ==='Login'? <button type='submit'>Daxil ol</button>:<button type='submit' onClick={()=>{setAction("Login")}}>Qeydiyyatdan keç</button>}
         <div className="register-link">
-            <p>Hesabınız yoxdur?</p>
-            <a href="#">Qeydiyyatdan keçin</a>
+            
+            {action ==='Sign up'?<div></div>:<a href="#" onClick={()=>{setAction("Sign up")}}>Qeydiyyatdan keçin</a>}
+            {action ==='Login'?<p>Hesabınız yoxdur?</p>:<div></div>}
+            
         </div>
       </form>
     </div>
